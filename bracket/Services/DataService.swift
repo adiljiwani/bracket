77d25956ba66247b547dd5e8f30c1d83887c9045
Adil Jiwani <a8jiwani@uwaterloo.ca>
2017-11-13 13:52:56 -0500

    fixed firebase connection

--- bracket/Services/DataService.swift
+++ bracket/Services/DataService.swift
@@ -13,4 +13,7 @@ let DB_BASE = Database.database().reference()
 
 class DataService {
     static let instance = DataService()
+    
+    private var _REF_BASE = DB_BASE
+    private var _REF_USERS = DB_BASE.child("users")
 }
