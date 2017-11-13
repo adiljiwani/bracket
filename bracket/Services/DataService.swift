c710ba8ef35df9e8f6cd13e27176dcd056aa230f
Adil Jiwani <a8jiwani@uwaterloo.ca>
2017-11-13 13:45:54 -0500

    init data service

---
+++ bracket/Services/DataService.swift
@@ -0,0 +1,16 @@
+//
+//  DataService.swift
+//  bracket
+//
+//  Created by Adil Jiwani on 2017-11-13.
+//  Copyright © 2017 Adil Jiwani. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+let DB_BASE = Database.database().reference()
+
+class DataService {
+    static let instance = DataService()
+}
