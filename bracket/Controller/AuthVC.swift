f334654441be9e97df8186ef3a337f6b8609272f
Adil Jiwani <a8jiwani@uwaterloo.ca>
2017-11-16 16:29:51 -0500

    logout user and dismiss vc

--- bracket/Controller/AuthVC.swift
+++ bracket/Controller/AuthVC.swift
@@ -7,14 +7,21 @@
 //
 
 import UIKit
+import Firebase
 
 class AuthVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        if Auth.auth().currentUser != nil {
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
     @IBAction func signInWithEmailPressed(_ sender: Any) {
         let loginVC = storyboard?.instantiateViewController(withIdentifier: "LoginVC")
         present(loginVC!, animated: true, completion: nil)
